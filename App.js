43d9039592b22453e81f3dd953d6357c87cd4bff
shobha98 <shrivastava.shobha@daffodilsw.com>
2021-05-12 13:29:13 +0530

    remove commented code

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
-// import PushNotification from 'react-native-push-notification';
 import firebase from '@react-native-firebase/app';
 import SplashScreen from 'react-native-splash-screen';
 
@@ -13,30 +12,6 @@ firebase.initializeApp();
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
-
-    // PushNotification.configure({
-    //   onRegister: function (token) {
-    //     console.log('TOKEN:', token);
-    //   },
-
-    //   onNotification: function (notification) {
-    //     console.log('NOTIFICATION:', notification);
-    //     if (notification.foreground) {
-    //       PushNotification.localNotification({
-    //         title: notification.title,
-    //         message: notification.message,
-    //       });
-    //     }
-    //   },
-
-    //   permissions: {
-    //     alert: true,
-    //     badge: true,
-    //     sound: true,
-    //   },
-    //   popInitialNotification: true,
-    //   requestPermissions: true,
-    // });
   }, []);
 
   return (
